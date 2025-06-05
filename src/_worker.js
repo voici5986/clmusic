622cc8f88fc3b1820e0c3aa1e5f8bb7f2ae46eef
voici5986 <qq475849258@gmail.com>
2025-06-05 14:32:56 +0700

    Update _worker.js

--- src/_worker.js
+++ src/_worker.js
@@ -91,3 +91,8 @@ function handleCorsPreflight(request) {
   if (requestedHeaders) {
     headers.set('Access-Control-Allow-Headers', requestedHeaders);
   } else {
+    headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+  }
+  headers.set('Access-Control-Max-Age', '86400');
+  return new Response(null, { status: 204, headers: headers });
+}
